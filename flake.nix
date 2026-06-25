ba72f566e56d1183f97cad2788cfd918e221b213
casey <caseyriley94@gmail.com>
2026-06-25 10:10:13 -0400

    archive

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     flake-parts = {
       url = "github:removed-user/flake-parts/Add-a-Check-in-mkTransposedPersystemModule";
+      # url = "path:../flake-parts/";
       inputs.nixpkgs-lib.follows = "nixpkgs-lib";
     };
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
@@ -13,38 +14,41 @@
     flake-parts,
     ...
   }:
-  /*
+  /**
   # 1. Keep your definitions flat and visible at the very top
   - Should Produce `Mylib.lib`.
   - A module argument you can pass around like lib, flake-parts-lib, etc.
   - Importable by users as seen below through
-  *imports = [${yourFlakeName}.myCustomModule];*
+  # *imports = [${yourFlakeName}.myCustomModule];*
   */
-  # let
-  # inherit nixpkgs-lib;
-  # lib = nixpkgs-lib.lib;
-  # myCustomModule = {lib, ...}: {
-  #   _class = "generic";
-  #   _module.args.Mylib = {
-  #     lib1 = import ./flakeModule.nix {inherit lib;}; # import a/the default flakeModule, and load lib from it
-  #     #or
-  #     lib2.define_monad = rec {
-  #       whatsAMonad = self: ''A Monad is just a monoid in the category of endofunctors'';
-  #       __functor = self: whatsAMonad;
-  #     };
-  #   };
-  # };
-  # in
-  /*
-  # 2. You "may" want to define a system type if necessary.
-     -  For pure nix-lib outputs, it's not required, and **can cause** issues for consumers.
-      - If you're not exporting custom packages/modules/overlays, then you probably don't actually need it
-  */
+  let
+    inherit (nixpkgs-lib) lib;
+    # lib = nixpkgs-lib.lib;
+    myCustomModule = {lib, ...}: {
+      _class = "generic";
+      _module.args.Mylib = {
+        lib1 = import ./flakeModule.nix {inherit lib;}; # import a/the default flakeModule, and load lib from it
+        #or
+        lib2.define_monad = rec {
+          whatsAMonad = self: ''A Monad is just a monoid in the category of endofunctors'';
+          __functor = self: whatsAMonad;
+        };
+      };
+    };
+  in
+    /**
+    # 2. You "may" want to define a system type if necessary.
+       -  For pure nix-lib outputs, it's not required, and **can cause** issues for consumers.
+        - If you're not exporting custom packages/modules/overlays, then you probably don't actually need it
+    */
     flake-parts.lib.mkFlake {
-      inherit (nixpkgs-lib) lib;
+      inherit
+        inputs
+        myCustomModule
+        ;
       #  systems = [ "x86_64-linux" ];
 
-      /*
+      /**
       # Safely Dogfood "use" your module internally; (for testing);
        >  please don't use overrides in-order to use your lib in your lib
        -  Without polluting consumers inputs
@@ -61,7 +65,7 @@
       ];
 
       imports = [
-        # myCustomModule
+        myCustomModule
         inputs.flake-parts.flakeModules.flakeModules
         inputs.flake-parts.flakeModules.modules
         inputs.flake-parts.flakeModules.debug
